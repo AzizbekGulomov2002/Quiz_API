4913c0ce52d8fcd4ba1cae803571d1ba2d60bda0
Azizbek <azizbekgulomov>
2022-08-09 19:37:22 +0500

    'deploy_heroku_app'

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,16 @@
 # from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # from quiz.views import getsavol
-
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    
 ]
+urlpatterns+=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
